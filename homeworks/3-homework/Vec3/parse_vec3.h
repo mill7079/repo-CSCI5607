d1f8fc38b51aa556aaf7215b6438085020664eaf
Allison Miller <mill7079@umn.edu>
2021-03-03 22:58:57 -0600

    initial parsing code for hw3

--- homeworks/3-homework/Vec3/parse_vec3.h
+++ homeworks/3-homework/Vec3/parse_vec3.h
@@ -10,14 +10,14 @@
 #include <fstream>
 #include <cstring>
 
-//Camera & Scene Parmaters (Global Variables)
+//Camera & Scene Parameters (Global Variables)
 //Here we set default values, override them in parseSceneFile()
 
-//Image Parmaters
+//Image Parameters
 int img_width = 800, img_height = 600;
 std::string imgName = "raytraced.png";
 
-//Camera Parmaters
+//Camera Parameters
 vec3 eye = vec3(0,0,0); 
 vec3 forward = vec3(0,0,-1).normalized();
 vec3 up = vec3(0,1,0).normalized();
@@ -30,6 +30,62 @@ float sphereRadius = 1;
 
 void parseSceneFile(std::string fileName){
   //TODO: Override the default values with new data from the file "fileName"
+   std::ifstream input(fileName.c_str());
+   if (input.fail()) {
+      std::cout << "Can't open file " << fileName << std::endl;
+      return;
+   }
+   
+   std::string line, word;
+   int count = 0;
+   while (input >> word) {
+      std::cout << "line " << count << ": " << word << std::endl;
+      if (word.length() >= 1 && word[0] == '#') {  // skip comments
+         std::cout << "comment" << std::endl;
+         getline(input, line);
+         count++;
+         continue;
+      }
+      
+      if (word == "sphere:") {
+         std::cout << "sphere" << std::endl;
+         input >> spherePos.x >> spherePos.y >> spherePos.z >> sphereRadius;
+         std::cout << "new sphere pos: " << spherePos.x << " " << spherePos.y <<
+         " " << spherePos.z << std::endl;
+         std::cout << "new sphere radius: " << sphereRadius << std::endl;
+      } else if (word == "camera_pos:") {
+         std::cout << "camera pos" << std::endl;
+         input >> eye.x >> eye.y >> eye.z;
+         std::cout << "new eye: " << eye.x << " " << eye.y << " " << eye.z << std::endl;
+      } else if (word == "camera_fwd:") {
+         std::cout << "camera fwd" << std::endl;
+         input >> forward.x >> forward.y >> forward.z;
+         std::cout << "new forward: " << forward.x << " " << forward.y << " " << forward.z << std::endl;
+         forward = forward.normalized();
+      } else if (word == "camera_up:") {
+         std::cout << "camera up" << std::endl;
+         input >> up.x >> up.y >> up.z;
+         std::cout << "new up: " << up.x << " " << up.y << " " << up.z << std::endl;
+         up = up.normalized();
+      } else if (word == "camera_fov_ha:") {
+         std::cout << "fov" << std::endl;
+         input >> halfAngleVFOV;
+         std::cout << "new fov: " << halfAngleVFOV << std::endl;
+      } else if (word == "image_resolution:") {
+         std::cout << "resolution" << std::endl;
+         input >> img_width >> img_height;
+         std::cout << "new width: " << img_width << " new height: " << img_height << std::endl;
+      } else if (word == "output_image:") {
+         std::cout << "output image" << std::endl;
+         input >> imgName;
+         std::cout << "new image name: " << imgName << std::endl;
+      } else {
+         std::cout << "Command not recognized: " << word << std::endl;
+      }
+      
+      count++;
+   
+   }
 
 
   //TODO: Create an orthagonal camera basis, based on the provided up and right vectors
