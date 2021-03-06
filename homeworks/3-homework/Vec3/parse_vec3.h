cc40c945dfc9b8ea16490ffb914358241a8e6ac8
Allison Miller <mill7079@umn.edu>
2021-03-05 20:36:24 -0600

    add orthogonalization

--- homeworks/3-homework/Vec3/parse_vec3.h
+++ homeworks/3-homework/Vec3/parse_vec3.h
@@ -24,7 +24,7 @@ vec3 up = vec3(0,1,0).normalized();
 vec3 right = vec3(-1,0,0).normalized();
 float halfAngleVFOV = 35; 
 
-//Scene (Sphere) Parmaters
+//Scene (Sphere) Parameters
 vec3 spherePos = vec3(0,0,2);
 float sphereRadius = 1; 
 
@@ -39,46 +39,46 @@ void parseSceneFile(std::string fileName){
    std::string line, word;
    int count = 0;
    while (input >> word) {
-      std::cout << "line " << count << ": " << word << std::endl;
+//      std::cout << "line " << count << ": " << word << std::endl;
       if (word.length() >= 1 && word[0] == '#') {  // skip comments
-         std::cout << "comment" << std::endl;
+//         std::cout << "comment" << std::endl;
          getline(input, line);
-         count++;
+//         count++;
          continue;
       }
       
       if (word == "sphere:") {
-         std::cout << "sphere" << std::endl;
+//         std::cout << "sphere" << std::endl;
          input >> spherePos.x >> spherePos.y >> spherePos.z >> sphereRadius;
-         std::cout << "new sphere pos: " << spherePos.x << " " << spherePos.y <<
-         " " << spherePos.z << std::endl;
-         std::cout << "new sphere radius: " << sphereRadius << std::endl;
+//         std::cout << "new sphere pos: " << spherePos.x << " " << spherePos.y <<
+//         " " << spherePos.z << std::endl;
+//         std::cout << "new sphere radius: " << sphereRadius << std::endl;
       } else if (word == "camera_pos:") {
-         std::cout << "camera pos" << std::endl;
+//         std::cout << "camera pos" << std::endl;
          input >> eye.x >> eye.y >> eye.z;
-         std::cout << "new eye: " << eye.x << " " << eye.y << " " << eye.z << std::endl;
+//         std::cout << "new eye: " << eye.x << " " << eye.y << " " << eye.z << std::endl;
       } else if (word == "camera_fwd:") {
-         std::cout << "camera fwd" << std::endl;
+//         std::cout << "camera fwd" << std::endl;
          input >> forward.x >> forward.y >> forward.z;
-         std::cout << "new forward: " << forward.x << " " << forward.y << " " << forward.z << std::endl;
+//         std::cout << "new forward: " << forward.x << " " << forward.y << " " << forward.z << std::endl;
          forward = forward.normalized();
       } else if (word == "camera_up:") {
-         std::cout << "camera up" << std::endl;
+//         std::cout << "camera up" << std::endl;
          input >> up.x >> up.y >> up.z;
-         std::cout << "new up: " << up.x << " " << up.y << " " << up.z << std::endl;
+//         std::cout << "new up: " << up.x << " " << up.y << " " << up.z << std::endl;
          up = up.normalized();
       } else if (word == "camera_fov_ha:") {
-         std::cout << "fov" << std::endl;
+//         std::cout << "fov" << std::endl;
          input >> halfAngleVFOV;
-         std::cout << "new fov: " << halfAngleVFOV << std::endl;
+//         std::cout << "new fov: " << halfAngleVFOV << std::endl;
       } else if (word == "image_resolution:") {
-         std::cout << "resolution" << std::endl;
+//         std::cout << "resolution" << std::endl;
          input >> img_width >> img_height;
-         std::cout << "new width: " << img_width << " new height: " << img_height << std::endl;
+//         std::cout << "new width: " << img_width << " new height: " << img_height << std::endl;
       } else if (word == "output_image:") {
-         std::cout << "output image" << std::endl;
+//         std::cout << "output image" << std::endl;
          input >> imgName;
-         std::cout << "new image name: " << imgName << std::endl;
+//         std::cout << "new image name: " << imgName << std::endl;
       } else {
          std::cout << "Command not recognized: " << word << std::endl;
       }
@@ -88,11 +88,31 @@ void parseSceneFile(std::string fileName){
    }
 
 
-  //TODO: Create an orthagonal camera basis, based on the provided up and right vectors
-  printf("Orthagonal Camera Basis:\n");
-  printf("forward: %f,%f,%f\n",forward.x,forward.y,forward.z);
-  printf("right: %f,%f,%f\n",right.x,right.y,right.z);
-  printf("up: %f,%f,%f\n",up.x,up.y,up.z);
+  //TODO: Create an orthogonal camera basis, based on the provided up and right vectors
+   
+   float dotProd = dot(forward, up);
+   if (dotProd == 0) {  // already orthogonal :)
+      std::cout << "orthogonal" << std::endl;
+   } else if (dotProd == 1) {  // parallel? i think?
+      std::cout << "ERROR: Forward and Up vectors cannot be parallel" << std::endl;
+      forward = vec3(0,0,-1).normalized();
+      up = vec3(0,1,0).normalized();
+   } else {  // orthogonalize
+      vec3 u1 = forward.normalized();
+      up = up.normalized();
+      up = up - (dot(u1, up) / dot(u1, u1)) * u1;
+      
+      up = up.normalized();
+      forward = forward.normalized();
+   }
+   
+   // find right
+   right = cross(up, forward).normalized();
+   
+   printf("Orthogonal Camera Basis:\n");
+   printf("forward: %f,%f,%f\n",forward.x,forward.y,forward.z);
+   printf("right: %f,%f,%f\n",right.x,right.y,right.z);
+   printf("up: %f,%f,%f\n",up.x,up.y,up.z);
 }
 
 #endif
