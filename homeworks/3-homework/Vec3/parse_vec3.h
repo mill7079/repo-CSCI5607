95919912eb8ec7ea5af1b75b30f986d35bbbef71
Allison Miller <mill7079@umn.edu>
2021-03-05 22:59:54 -0600

    remove commented print statements

--- homeworks/3-homework/Vec3/parse_vec3.h
+++ homeworks/3-homework/Vec3/parse_vec3.h
@@ -39,46 +39,30 @@ void parseSceneFile(std::string fileName){
    std::string line, word;
    int count = 0;
    while (input >> word) {
-//      std::cout << "line " << count << ": " << word << std::endl;
-      if (word.length() >= 1 && word[0] == '#') {  // skip comments
-//         std::cout << "comment" << std::endl;
+      
+      // skip comments
+      if (word.length() >= 1 && word[0] == '#') {
          getline(input, line);
-//         count++;
          continue;
       }
       
+      // really wish c++ switch statements worked on strings...
       if (word == "sphere:") {
-//         std::cout << "sphere" << std::endl;
          input >> spherePos.x >> spherePos.y >> spherePos.z >> sphereRadius;
-//         std::cout << "new sphere pos: " << spherePos.x << " " << spherePos.y <<
-//         " " << spherePos.z << std::endl;
-//         std::cout << "new sphere radius: " << sphereRadius << std::endl;
       } else if (word == "camera_pos:") {
-//         std::cout << "camera pos" << std::endl;
          input >> eye.x >> eye.y >> eye.z;
-//         std::cout << "new eye: " << eye.x << " " << eye.y << " " << eye.z << std::endl;
       } else if (word == "camera_fwd:") {
-//         std::cout << "camera fwd" << std::endl;
          input >> forward.x >> forward.y >> forward.z;
-//         std::cout << "new forward: " << forward.x << " " << forward.y << " " << forward.z << std::endl;
          forward = forward.normalized();
       } else if (word == "camera_up:") {
-//         std::cout << "camera up" << std::endl;
          input >> up.x >> up.y >> up.z;
-//         std::cout << "new up: " << up.x << " " << up.y << " " << up.z << std::endl;
          up = up.normalized();
       } else if (word == "camera_fov_ha:") {
-//         std::cout << "fov" << std::endl;
          input >> halfAngleVFOV;
-//         std::cout << "new fov: " << halfAngleVFOV << std::endl;
       } else if (word == "image_resolution:") {
-//         std::cout << "resolution" << std::endl;
          input >> img_width >> img_height;
-//         std::cout << "new width: " << img_width << " new height: " << img_height << std::endl;
       } else if (word == "output_image:") {
-//         std::cout << "output image" << std::endl;
          input >> imgName;
-//         std::cout << "new image name: " << imgName << std::endl;
       } else {
          std::cout << "Command not recognized: " << word << std::endl;
       }
@@ -107,6 +91,8 @@ void parseSceneFile(std::string fileName){
    }
    
    // find right
+   // i thought this should be forward x up?? but that reverses the image from the given example
+   // so i'm not really sure what to think
    right = cross(up, forward).normalized();
    
    printf("Orthogonal Camera Basis:\n");
